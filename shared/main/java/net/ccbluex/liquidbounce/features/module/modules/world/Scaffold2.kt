de0ba794d54514dee75b2f2d3212afc56cdf527b
memsss <aggelos.bilai73@gmail.com>
2021-01-10 22:20:17 +0200

    Created Scaffold2

--- shared/main/java/net/ccbluex/liquidbounce/features/module/modules/world/Scaffold2.kt
+++ shared/main/java/net/ccbluex/liquidbounce/features/module/modules/world/Scaffold2.kt
@@ -5,7 +5,7 @@ import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
 import org.lwjgl.input.Keyboard
 
-@ModuleInfo(name = "Scaffold", description = "Automatically places blocks beneath your feet.", category = ModuleCategory.WORLD, keyBind = Keyboard.KEY_I)
+@ModuleInfo(name = "Scaffold2", description = "Automatically places blocks beneath your feet.", category = ModuleCategory.WORLD, keyBind = Keyboard.KEY_I)
 
 class Scaffold2: Module() {
 }
